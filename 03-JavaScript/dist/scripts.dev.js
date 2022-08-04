67e9e9f0727fec250d25a18cb89aa16b798d4b2b
Gar-Yin Lam <garyin.lam@gmail.com>
2022-08-04 17:26:16 +0100

    Challenge 2

--- 03-JavaScript/dist/scripts.dev.js
+++ 03-JavaScript/dist/scripts.dev.js
@@ -12,3 +12,14 @@ function isNumberPositive(number) {
 
   return isPositive;
 }
+
+console.log(isNumberPositive(-1));
+console.log(isNumberPositive(10)); //2
+
+function convertDaysToAge(days) {
+  var years = Math.floor(days / 365);
+  return years;
+}
+
+console.log(convertDaysToAge(3650));
+console.log(convertDaysToAge(6570));
--- 03-JavaScript/scripts.js
+++ 03-JavaScript/scripts.js
@@ -9,3 +9,16 @@ function isNumberPositive (number) {
   }
   return isPositive;
 }
+
+console.log(isNumberPositive(-1));
+console.log(isNumberPositive(10));
+
+//2
+
+function convertDaysToAge (days) {
+  const years = Math.floor(days/365);
+  return years;
+}
+
+console.log(convertDaysToAge(3650));
+console.log(convertDaysToAge(6570));
